c987da93ae57ce76fc75fd43b82459eb9047d689
harkamalsi <harkamal_guraya@hotmail.com>
2020-02-21 17:36:19 +0100

    Followed a tutorial, connected spring boot with  mongoDB atlas and tested with postman; see the wiki or readme for tutorial link.

---
+++ src/main/java/com/example/developerforum/DeveloperforumApplication.java
@@ -0,0 +1,13 @@
+package com.example.developerforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/developerforum/DeveloperforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developerforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
